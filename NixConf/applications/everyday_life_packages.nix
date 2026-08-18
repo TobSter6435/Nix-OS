c9b4fe11f9f8b88775f73736717e76081bc9bd4f
TobSter6435 <tepi2499@gmx.de>
2026-08-19 01:52:58 +0200

    .

--- NixConf/applications/everyday_life_packages.nix
+++ NixConf/applications/everyday_life_packages.nix
@@ -1,22 +1,43 @@
 { config, pkgs, ... }:
 
 {
+
+  services.openssh.enable = true;
+
+  services.tailscale.enable = true;
+
+  boot.kernelModules = [ "wireguard" ];
+
+  
+
   programs.steam = {
+
     enable = true;
+
     remotePlay.openFirewall = true;
+
     dedicatedServer.openFirewall = true;
+
   };
 
 
+
+  programs.gpu-screen-recorder.enable = true;
+
   programs.fish.enable = true;
+
   services.mullvad-vpn.enable = true;
+
   services.flatpak.enable = true;
+
+
 
   environment.systemPackages = with pkgs; [
     mullvad-browser
     mullvad-vpn
     vesktop
     kitty
+    wine
     starship
     btop
     pavucontrol
@@ -34,6 +55,11 @@
     tty-clock
     mapscii
     pipes
+    gpu-screen-recorder-gtk
+    keepassxc
+    tailscale
+    localsend
+    jan
   ];
-}
 
+}
